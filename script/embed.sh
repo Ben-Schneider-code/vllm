f2e6a9a069a9bea1fae5a66b4377069e281af0ed
Ben Schneider <schnei19@myumanitoba.ca>
2024-10-17 16:58:59 +0000

    Finish migration away from using deepspeed for embeddings.

--- script/embed.sh
+++ script/embed.sh
@@ -1,4 +1,25 @@
-echo "USE GPU:$2 TO EMBED CONFIG $1"
+if [ "$1" ]; then
+  CONFIG=$1
+else
+  echo "MUST PROVIDE A CONFIG"
+fi
+
+if [ "$2" ]; then
+  echo "USING GPU:$2"
+  GPU=$2
+else
+  echo "DEFAULTING TO GPU:0"
+  GPU="0"
+fi
+
+
+if [ "$3" ]; then
+  echo "USING TOP-K DEPTH OF $3"
+  TOPK=$3
+else
+  echo "USING DEFAULT TOP-K DEPTH OF 10"
+  TOPK="10"
+fi
+
 cd $VLLM
-#deepspeed --include localhost:$2 $VLLM/visualization/embed.py $VLLM/config/embed/$1
-python 
+CUDA_VISIBLE_DEVICES=$GPU python $VLLM/visualization/embed_.py $VLLM$CONFIG $TOPK

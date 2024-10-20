f6565868a56778e3c2b09742d33a68c8c6b3f502
Ben Schneider <schnei19@myumanitoba.ca>
2024-10-20 20:38:50 +0000

    Add two version of MLP adapter.

--- internvl/model/abc/modeling_abc.py
+++ internvl/model/abc/modeling_abc.py
@@ -106,11 +106,185 @@ class IVLTCOS(InternVLChatModel):
             }
         return (loss, outputs) if return_outputs or return_prediction else loss
 
+class IVLTCOS(InternVLChatModel):
+    """
+    Added scaling to the contrastive loss and optimize the scaling param.
+    and label smoothing.
+    """
+    
+    attn_mask = "bidirectional"
+
+    def __init__(self, config, *args, **kwargs):
+        super().__init__(config, *args, **kwargs)
+        self.temperature = torch.nn.Parameter(torch.tensor(0.1,
+                                                            requires_grad=True,
+                                                            dtype=torch.float32))
+        
+
+
+    def forward(self, inputs, return_outputs=False, return_prediction=False):
+        query = inputs["query"]
+        candidate = inputs["pos_cand"]
+        query.pop("labels")
+        candidate.pop("labels")
+
+        query_outputs : CausalLMOutputWithPast = super().forward(**query, output_hidden_states=True)
+        candidate_outputs : CausalLMOutputWithPast = super().forward(**candidate, output_hidden_states=True)
+        
+        # ensure logits computation was skipped for memory / speed
+        # (or other LLM in used)
+        assert(query_outputs.logits is None)
+        assert(candidate_outputs.logits is None)
+
+        q_eos_token_emb = get_mean_token_embed(query["input_ids"], query_outputs.hidden_states[-1], 0)
+        c_eos_token_emb= get_mean_token_embed(candidate["input_ids"], candidate_outputs.hidden_states[-1], 0)
+        
+        q_emb = q_eos_token_emb.float()
+        c_emb = c_eos_token_emb.float()
+        q_emb = F.normalize(q_emb, dim=-1)
+        c_emb = F.normalize(c_emb, dim=-1)
+
+        loss, acc = compute_contrastive_loss(q_emb, c_emb, temperature=self.temperature.float(), label_smoothing=0.1)
+        
+        outputs = {}
+        if return_outputs:
+            outputs["accuracy"] = acc
+            outputs["temperature"] = self.temperature
+        if return_prediction:
+            outputs["prediction"] = {
+                "meta": inputs["meta"],
+                "q": q_emb.detach().cpu(),
+                "c": c_emb.detach().cpu()
+            }
+        return (loss, outputs) if return_outputs or return_prediction else loss
+
+class MLP(torch.nn.Module):
+
+    def __init__(self, hidden_size: int):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.linear_layer1 = torch.nn.Linear(hidden_size, hidden_size)
+        self.linear_layer2 = torch.nn.Linear(hidden_size, hidden_size)
+        self.gelu = torch.nn.GELU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.linear_layer1(x)
+        x = self.gelu(x)
+        x = self.linear_layer2(x)
+        return x
+
+class IVLMLP(InternVLChatModel):
+    """
+    Added scaling to the contrastive loss and optimize the scaling param.
+    and label smoothing.
+    and MLP projection layer.
+    """
+    
+    attn_mask = "bidirectional"
+
+    def __init__(self, config, *args, **kwargs):
+        super().__init__(config, *args, **kwargs)
+        self.mlp_head = MLP(config.hidden_size)
+        self.temperature = torch.nn.Parameter(torch.tensor(0.1,
+                                                            requires_grad=True,
+                                                            dtype=torch.float32))
+        
+    def forward(self, inputs, return_outputs=False, return_prediction=False):
+        query = inputs["query"]
+        candidate = inputs["pos_cand"]
+        query.pop("labels")
+        candidate.pop("labels")
+
+        query_outputs : CausalLMOutputWithPast = super().forward(**query, output_hidden_states=True)
+        candidate_outputs : CausalLMOutputWithPast = super().forward(**candidate, output_hidden_states=True)
+        
+        # ensure logits computation was skipped for memory / speed
+        # (or other LLM in used)
+        assert(query_outputs.logits is None)
+        assert(candidate_outputs.logits is None)
+
+        q_eos_token_emb = get_mean_token_embed(query["input_ids"], query_outputs.hidden_states[-1], 0)
+        c_eos_token_emb= get_mean_token_embed(candidate["input_ids"], candidate_outputs.hidden_states[-1], 0)
+        
+        q_emb = self.mlp_head(q_eos_token_emb).float()
+        c_emb = self.mlp_head(c_eos_token_emb).float()
+        q_emb = F.normalize(q_emb, dim=-1)
+        c_emb = F.normalize(c_emb, dim=-1)
+
+        loss, acc = compute_contrastive_loss(q_emb, c_emb, temperature=self.temperature.float(), label_smoothing=0.1)
+        
+        outputs = {}
+        if return_outputs:
+            outputs["accuracy"] = acc
+            outputs["temperature"] = self.temperature
+        if return_prediction:
+            outputs["prediction"] = {
+                "meta": inputs["meta"],
+                "q": q_emb.detach().cpu(),
+                "c": c_emb.detach().cpu()
+            }
+        return (loss, outputs) if (return_outputs or return_prediction) else loss
+
+class IVLMLP2(InternVLChatModel):
+    """
+    Added scaling to the contrastive loss and optimize the scaling param.
+    and label smoothing.
+    and MLP projection layer.
+    """
+    
+    attn_mask = "bidirectional"
+
+    def __init__(self, config, *args, **kwargs):
+        super().__init__(config, *args, **kwargs)
+        self.mlp_q = MLP(config.hidden_size)
+        self.mlp_c = MLP(config.hidden_size)
+        self.temperature = torch.nn.Parameter(torch.tensor(0.1,
+                                                            requires_grad=True,
+                                                            dtype=torch.float32))
+        
+    def forward(self, inputs, return_outputs=False, return_prediction=False):
+        query = inputs["query"]
+        candidate = inputs["pos_cand"]
+        query.pop("labels")
+        candidate.pop("labels")
+
+        query_outputs : CausalLMOutputWithPast = super().forward(**query, output_hidden_states=True)
+        candidate_outputs : CausalLMOutputWithPast = super().forward(**candidate, output_hidden_states=True)
+        
+        # ensure logits computation was skipped for memory / speed
+        # (or other LLM in used)
+        assert(query_outputs.logits is None)
+        assert(candidate_outputs.logits is None)
+
+        q_eos_token_emb = get_mean_token_embed(query["input_ids"], query_outputs.hidden_states[-1], 0)
+        c_eos_token_emb= get_mean_token_embed(candidate["input_ids"], candidate_outputs.hidden_states[-1], 0)
+        
+        q_emb = self.mlp_q(q_eos_token_emb).float()
+        c_emb = self.mlp_c(c_eos_token_emb).float()
+        q_emb = F.normalize(q_emb, dim=-1)
+        c_emb = F.normalize(c_emb, dim=-1)
+
+        loss, acc = compute_contrastive_loss(q_emb, c_emb, temperature=self.temperature.float(), label_smoothing=0.1)
+        
+        outputs = {}
+        if return_outputs:
+            outputs["accuracy"] = acc
+            outputs["temperature"] = self.temperature
+        if return_prediction:
+            outputs["prediction"] = {
+                "meta": inputs["meta"],
+                "q": q_emb.detach().cpu(),
+                "c": c_emb.detach().cpu()
+            }
+        return (loss, outputs) if (return_outputs or return_prediction) else loss
+
+
 class IVLTGS(InternVLChatModel):
     """
     Added scaling to the contrastive loss and optimize the scaling param.
     and label smoothing
     and loss gathering.
+    TODO multi-gpu metrics gathering needs to be fixed
     """
     
     attn_mask = "bidirectional"
@@ -168,4 +342,6 @@ MODEL_ARCHITECTURE = {
     "IVLTCO": IVLTCO,
     "IVLTCOS": IVLTCOS,
     "IVLTGS": IVLTGS,
+    "IVLMLP": IVLMLP,
+    "IVLMLP2": IVLMLP2
  }
--- leaderboard.md
+++ leaderboard.md
@@ -1,7 +1,8 @@
 
 Relative score                          CC   |   MSCOCO 
-1. Internvl-Baseline @ 50 -  
-2. Internvl-Baseline @ 1000 - 
-3. Internvl-large-batch-size @ 100
-4. Internvl-large-batch-size @ len(CC)
-5. Internvl-8B - @ XXX
+1. IVLTCOS - InternVL-8B
+2. IVLTCOS - InternVL-1B
+3. IVLTCO - InternVL-1B
+4.
+
+Gathered results?
